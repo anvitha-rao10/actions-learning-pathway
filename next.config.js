ff7d221ad3cb1fc6dc58ce41f4896d9696e4cccc
Anvitha G Rao <119688233+anvitha-rao10@users.noreply.github.com>
2025-10-30 05:32:18 +0000

    Adding next.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-
   images: {
-    unoptimized: true,
-  },
-  
-  reactStrictMode: true,
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
